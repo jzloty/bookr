320001d43362c0ed4e84ff6a6202f86e302d47f3
Dadits <goldenbiq@gmail.com>
2022-06-28 12:24:15 +0200

    Przeszukiwanie na stronie book-search

--- bookr/bookr/urls.py
+++ bookr/bookr/urls.py
@@ -21,4 +21,5 @@ import reviews.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', reviews.views.index),
+    path('search-book', reviews.views.szukanie)
 ]
--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 def index(request):
     name = "World!"
     return render(request, "base.html", {"name": name})
+
+
+def szukanie(request):
+    szukana = request.GET.get("szukana")
+    context = {"szukana": szukana}
+    return render(request, "book-search.html", context)
